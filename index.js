ce51b9d0ea9c05198496a496d4c93394e48e8a55
AbdalrahmanM <abdulrahman.al-samaraie@ogr.sakarya.edu.tr>
2023-04-27 21:57:30 +0300

    Assignment Completed

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Write your solution in this file!
+var customerName='bob'
+function upperCaseCustomerName(name){
+    return customerName=customerName.toUpperCase()
+}
+function setBestCustomer(){
+    return bestCustomer='not bob'
+}
+function overwriteBestCustomer(){
+    return bestCustomer='maybe bob'
+}
+const leastFavoriteCustomer='I ll be the best'
+function changeLeastFavoriteCustomer(){
+    return leastFavoriteCustomer='Assignment to constant variable.'
+}
